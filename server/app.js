7911eb92774f5340ea45e34375d8f23526cd8e68
*vivek <vrvivekofficial@gmail.com>
2025-07-20 06:03:53 +0530

    added dotenv

--- server/app.js
+++ server/app.js
@@ -1,29 +1,28 @@
 import express from "express";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import cors from "cors";
-import { error } from "node:console";
 import { connetToSocket } from "./controller/socketManager.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const io = connetToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.use(cors())
-app.use(express.json({ limit: "40kb" }))
-app.use(express.urlencoded({limit:"40kb",extended:true}))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello" });
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://vrvivekpc:vrvivekpc@zoomclone.2gh3bfa.mongodb.net/?retryWrites=true&w=majority&appName=zoomclone"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Mongo db is connected");
   })
